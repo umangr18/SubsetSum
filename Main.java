a84fa1bda3a86a96bf72182a68f8e47672cf985e
mariaparandia <arandia.m@northeastern.edu>
2022-05-16 22:43:15 -0400

    finalized project 1

--- Main.java
+++ Main.java
@@ -10,24 +10,28 @@ import java.util.concurrent.TimeoutException;
 
 class Main {
   public static void main(String[] args) {
+
     ArrayList<Pair> baseCases = Parse.parse("instances/BaseCases.txt");
     ArrayList<Pair> randomlyGenerated = Parse.parse("instances/RandomlyGenerated.txt");
     ArrayList<Pair> onlineResources = Parse.parse("instances/OnlineResources.txt");
     ArrayList<Pair> curatedHard = Parse.parse("instances/CuratedHard.txt");
 
+
     try {
       PrintWriter writer = new PrintWriter("ExhaustiveResults.txt", StandardCharsets.UTF_8);
       writer.println("ONE MINUTE RESULTS");
-      writer.println("Base Cases:\n" + runInstances(baseCases, 1) + "\n");
-      writer.println("Randomly Generated:\n" + runInstances(randomlyGenerated, 1) + "\n");
-      writer.println("Online Resources:\n" + runInstances(onlineResources, 1) + "\n");
-      writer.println("Curated Hard:\n" + runInstances(curatedHard, 1) + "\n");
+
+      writer.println(runInstances(baseCases, 1) + "\n");
+      writer.println(runInstances(randomlyGenerated, 1) + "\n");
+      writer.println(runInstances(onlineResources, 1) + "\n");
+      writer.println(runInstances(curatedHard, 1) + "\n");
 
       writer.println("TEN MINUTE RESULTS");
-      writer.println("Base Cases:\n" + runInstances(baseCases, 10) + "\n");
-      writer.println("Randomly Generated:\n" + runInstances(randomlyGenerated, 10) + "\n");
-      writer.println("Online Resources:\n" + runInstances(onlineResources, 10) + "\n");
-      writer.println("Curated Hard:\n" + runInstances(curatedHard, 10) + "\n");
+      writer.println(runInstances(baseCases, 10) + "\n");
+      writer.println(runInstances(randomlyGenerated, 10) + "\n");
+      writer.println(runInstances(onlineResources, 10) + "\n");
+      writer.println(runInstances(curatedHard, 10) + "\n");
+
       writer.close();
     } catch (IOException e) {
       System.out.println(e.getMessage());
@@ -49,7 +53,7 @@ class Main {
         //results.add(Boolean.toString(result));
         results.add(instance.getList().size() + "-" + (endTime - startTime)/1000000);
       } catch (TimeoutException ex) {
-        results.add("FAIL");
+        results.add(instance.getList().size() + "-" + "FAIL");
       } catch (Exception ex) {
         System.out.println(ex.getMessage());
       }
