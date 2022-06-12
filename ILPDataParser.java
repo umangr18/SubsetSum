26fdd92cc623ca3b3adfd722047cfd0d416408b1
Umang <umangrastogi18@gmail.com>
2022-06-12 15:44:21 -0400

    fix ILP parser

--- ILPDataParser.java
+++ ILPDataParser.java
@@ -1,35 +1,46 @@
 import java.util.ArrayList;
 
 /*
-- given an instance in the form "[x x x]:t"
+- given an instance in the form "[x ... x]:t"
 - return the contents of a dat file in the form ...
-     param n := 3;
-     param weight := x x x;
      param target := t;
+     set E := 1 ... n ;
+     param weight :=
+       1  x
+       .  x
+       .  x
+       .  x
+       n  x
+     ;
  */
 public class ILPDataParser {
 
   ArrayList<Integer> list;
   int target;
-  int n;
 
   ILPDataParser(Pair instance) {
     this.list = instance.getList();
     this.target = instance.getTarget();
-    this.n = this.list.size();
   }
 
-  private String printElements() {
+  private String set() {
     StringBuilder s = new StringBuilder();
-    for (int i : this.list) {
+    for (int i = 1; i <= this.list.size(); i++) {
       s.append(i).append(" ");
     }
     return s.toString();
   }
 
-  public String convert() {
-    return "param n := " + this.n + ";\nparam weight := " + this.printElements() + ";\nparam target := " + this.target + ";";
+  private String weight() {
+    StringBuilder s = new StringBuilder();
+    for (int i = 1; i <= this.list.size(); i++) {
+      s.append("  ").append(i).append("  ").append(this.list.get(i)).append("\n");
+    }
+    return s.toString();
   }
 
+  public String convert() {
+    return "param target := " + this.target + ";\nset E := " + this.set() + ";\nparam weight := " + this.weight() + ";";
+  }
 
 }
